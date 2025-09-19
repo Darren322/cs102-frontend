ef1eaa3b268399864a36390cce73ae9ccebdb2aa
Darren322 <xiia0_darren@hotmail.com>
2025-09-19 16:36:38 +0800

    added basename to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css"; // if using Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cs102-frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
