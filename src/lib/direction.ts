3b56b3946994a1ad497d5ca22359e2366aa11046
Darren322 <xiia0_darren@hotmail.com>
2025-10-21 22:10:08 +0800

    fix: face upload angle

--- src/lib/direction.ts
+++ src/lib/direction.ts
@@ -14,7 +14,7 @@ export type Direction =
     | "STRAIGHT"
     | "NO_FACE";
 const PITCH_UP_THRESHOLD = 100;
-const PITCH_DOWN_THRESHOLD = 140;
+const PITCH_DOWN_THRESHOLD = 136;
 const YAW_RIGHT_THRESHOLD = 60;
 const YAW_LEFT_THRESHOLD = 95;
 
